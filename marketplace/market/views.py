72e5562efb83824f32bca4471d3bf899b5721394
Xurron <corentin.boeglin2005@gmail.com>
2024-04-30 11:37:07 +0200

    v0.1.2

--- marketplace/market/views.py
+++ marketplace/market/views.py
@@ -1,19 +1,51 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import ItemForm
+from . import models
+from .models import Item
 
 def index(request):
-    return render(request, 'market/index.html')
+    Items = Item.objects.all()
+    return render(request, 'market/index.html', {'Items': Items})
 
-def items(request):
-    return render(request, 'market/items/item.html')
+def items(request, id):
+    Items = Item.objects.get(id=id)
+    return render(request, 'market/items/item.html', {'Items': Items})
 
 def add_item(request):
-    return render(request, 'market/items/add.html')
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            Item = form.save()
+            return render(request, 'market/items/item.html', {'Item': Item})
+        else:
+            return render(request, 'market/items/add.html', {'form': form})
+    else:
+        form = ItemForm()
+        return render(request, 'market/items/add.html', {'form': form})
+
+def traitement_add_item(request):
+    iform = ItemForm(request.POST)
+    if iform.is_valid():
+        Item = iform.save()
+        return render(request, 'market/items/item.html', {'Item': Item})
+    else:
+        return render(request, 'market/items/add.html', {'form': iform})
 
 def delete_item(request, id):
+    Items = Item.objects.get(id=id)
+    Items.delete()
     return render(request, 'market/items/delete.html')
 
 def update_item(request, id):
-    return render(request, 'market/items/update.html')
+    iform = ItemForm(request.POST)
+    if iform.is_valid():
+        Item = iform.save(commit=False)
+        Item.id = id
+        Item.save()
+        return HttpResponseRedirect('/market')
+    else:
+        return render(request, 'market/items/update.html', {'form': iform, 'id': id})
 
 def users(request):
     return render(request, 'market/users/user.html')
