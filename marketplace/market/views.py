5bea1507fadde890dc30bf6c9523475b740cdffd
Xurron <corentin.boeglin2005@gmail.com>
2024-04-30 11:58:28 +0200

    v0.1.3

--- marketplace/market/views.py
+++ marketplace/market/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import ItemForm
+from .forms import ItemForm, UserForm
 from . import models
 from .models import Item
+from .models import User
 
 def index(request):
     Items = Item.objects.all()
@@ -47,13 +48,33 @@ def update_item(request, id):
     else:
         return render(request, 'market/items/update.html', {'form': iform, 'id': id})
 
-def users(request):
-    return render(request, 'market/users/user.html')
+def users(request, id):
+    Users = User.objects.get(id=id)
+    return render(request, 'market/users/user.html', {'Users': Users})
 
 def add_user(request):
-    return render(request, 'market/users/add.html')
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            User = form.save()
+            return render(request, 'market/users/user.html', {'User': User})
+        else:
+            return render(request, 'market/users/add.html', {'form': form})
+    else:
+        form = UserForm()
+        return render(request, 'market/users/add.html', {'form': form})
+
+def traitement_add_user(request):
+    uform = UserForm(request.POST)
+    if uform.is_valid():
+        User = uform.save()
+        return render(request, 'market/users/user.html', {'User': User})
+    else:
+        return render(request, 'market/users/add.html', {'form': uform})
 
 def delete_user(request, id):
+    Users = User.objects.get(id=id)
+    Users.delete()
     return render(request, 'market/users/delete.html')
 
 def update_user(request, id):
