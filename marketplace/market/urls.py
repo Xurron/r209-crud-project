ac90bb821348868cc03496af14975d1913081238
Xurron <corentin.boeglin2005@gmail.com>
2024-04-30 10:38:10 +0200

    v0.1.1

--- marketplace/market/urls.py
+++ marketplace/market/urls.py
@@ -4,4 +4,14 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index),
+
+    path('index/items/', views.items),
+    path('index/items/add/', views.add_item),
+    path('index/items/delete/<int:id>/', views.delete_item),
+    path('index/items/update/<int:id>/', views.update_item),
+
+    path('index/users/', views.users),
+    path('index/users/add/', views.add_user),
+    path('index/users/delete/<int:id>/', views.delete_user),
+    path('index/users/update/<int:id>/', views.update_user),
 ]
--- marketplace/market/views.py
+++ marketplace/market/views.py
@@ -2,3 +2,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'market/index.html')
+
+def items(request):
+    return render(request, 'market/items/item.html')
+
+def add_item(request):
+    return render(request, 'market/items/add.html')
+
+def delete_item(request, id):
+    return render(request, 'market/items/delete.html')
+
+def update_item(request, id):
+    return render(request, 'market/items/update.html')
+
+def users(request):
+    return render(request, 'market/users/user.html')
+
+def add_user(request):
+    return render(request, 'market/users/add.html')
+
+def delete_user(request, id):
+    return render(request, 'market/users/delete.html')
+
+def update_user(request, id):
+    return render(request, 'market/users/update.html')
